bdc8dd84661346670ad1afee2367dafed662a89b
joshaljomon <joshal.jomon@my.jcu.edu.au>
2025-09-29 00:32:52 +1000

    Got rid of global variables in temperatures

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -25,11 +25,11 @@ Q - Quit"""
     print("Thank you.")
 
 
-def change_celsius_to_fahrenheit():
+def change_celsius_to_fahrenheit(celsius):
     return celsius * 9.0 / 5 + 32
 
 
-def change_fahrenheit_to_celsius():
+def change_fahrenheit_to_celsius(fahrenheit):
     return 5 / 9 * (fahrenheit - 32)
 
 
