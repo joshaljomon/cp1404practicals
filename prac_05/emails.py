e87af8324360afb91f20d0ea44e9ade2f2aec89a
joshaljomon <joshal.jomon@my.jcu.edu.au>
2025-10-19 23:12:19 +1000

    Create and complete emails.py

---
+++ prac_05/emails.py
@@ -0,0 +1,31 @@
+"""
+Emails
+Estimate: 15 mins
+Actual:
+"""
+
+def main():
+    """Create dictionary of emails_to_names"""
+    email_to_name = {}
+    email = input("Email: ")
+    while email != "":
+        name = get_name_from_email(email)
+        confirmation = input(f"Is your name {name}? (Y/n) ")
+        if confirmation.upper() !="Y" and confirmation != "":
+            name = input("Name: ")
+        email_to_name[email] = name
+        email = input("Email: ")
+
+    for email, name in email_to_name.items():
+            print(f"{name} ({email})")
+
+
+def get_name_from_email(email):
+    """Get name from email"""
+    username = email.split('@')[0]
+    parts = username.split('.')
+    name = " ".join(parts).title()
+    return name
+
+
+main()
